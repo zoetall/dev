5bda6e0231770c5a2ca8a78f81880a431e3e0d1f
zoetall <zoetall@live.unc.edu>
2022-08-25 07:43:59 -0400

    Initial commit

---
+++ blackpopchange.js
@@ -0,0 +1,414 @@
+var blackpopchange={ 
+    "meta": { 
+    "limit": 100,
+    "offset": 0,
+    "total_count": 100
+},
+    
+"objects": [
+ {
+        "AreaName": "Alamance",
+        "PercentChange": "19.9"
+    },
+    {
+        "AreaName": "Alexander",
+        "PercentChange": "-5.2"
+    },
+    {
+        "AreaName": "Alleghany",
+        "PercentChange": "-22.6"
+    },
+    {
+        "AreaName": "Anson",
+        "PercentChange": "-24.5"
+    },
+    {
+        "AreaName": "Ashe",
+        "PercentChange": "-15.5"
+    },
+    {
+        "AreaName": "Avery",
+        "PercentChange": "-8.1"
+    },
+    {
+        "AreaName": "Beaufort",
+        "PercentChange": "-15.8"
+    },
+    {
+        "AreaName": "Bertie",
+        "PercentChange": "-19.5"
+    },
+    {
+        "AreaName": "Bladen",
+        "PercentChange": "-22.1"
+    },
+    {
+        "AreaName": "Brunswick",
+        "PercentChange": "-6"
+    },
+    {
+        "AreaName": "Buncombe",
+        "PercentChange": "0.3"
+    },
+    {
+        "AreaName": "Burke",
+        "PercentChange": "-20"
+    },
+    {
+        "AreaName": "Cabarrus",
+        "PercentChange": "56.4"
+    },
+    {
+        "AreaName": "Caldwell",
+        "PercentChange": "-4.5"
+    },
+    {
+        "AreaName": "Camden",
+        "PercentChange": "-19.2"
+    },
+    {
+        "AreaName": "Carteret",
+        "PercentChange": "-19.5"
+    },
+    {
+        "AreaName": "Caswell",
+        "PercentChange": "-14.9"
+    },
+    {
+        "AreaName": "Catawba",
+        "PercentChange": "-1.8"
+    },
+    {
+        "AreaName": "Chatham",
+        "PercentChange": "-6.1"
+    },
+    {
+        "AreaName": "Cherokee",
+        "PercentChange": "14.1"
+    },
+    {
+        "AreaName": "Chowan",
+        "PercentChange": "-13.4"
+    },
+    {
+        "AreaName": "Clay",
+        "PercentChange": "-6.3"
+    },
+    {
+        "AreaName": "Cleveland",
+        "PercentChange": "-1"
+    },
+    {
+        "AreaName": "Columbus",
+        "PercentChange": "-17.5"
+    },
+    {
+        "AreaName": "Craven",
+        "PercentChange": "-13"
+    },
+    {
+        "AreaName": "Cumberland",
+        "PercentChange": "9"
+    },
+    {
+        "AreaName": "Currituck",
+        "PercentChange": "2.2"
+    },
+    {
+        "AreaName": "Dare",
+        "PercentChange": "-16.5"
+    },
+    {
+        "AreaName": "Davidson",
+        "PercentChange": "11"
+    },
+    {
+        "AreaName": "Davie",
+        "PercentChange": "-5.4"
+    },
+    {
+        "AreaName": "Duplin",
+        "PercentChange": "-21.9"
+    },
+    {
+        "AreaName": "Durham",
+        "PercentChange": "8.9"
+    },
+    {
+        "AreaName": "Edgecombe",
+        "PercentChange": "-15.5"
+    },
+    {
+        "AreaName": "Forsyth",
+        "PercentChange": "4.7"
+    },
+    {
+        "AreaName": "Franklin",
+        "PercentChange": "-1.3"
+    },
+    {
+        "AreaName": "Gaston",
+        "PercentChange": "28.3"
+    },
+    {
+        "AreaName": "Gates",
+        "PercentChange": "-25.2"
+    },
+    {
+        "AreaName": "Graham",
+        "PercentChange": "170.6"
+    },
+    {
+        "AreaName": "Granville",
+        "PercentChange": "-5.9"
+    },
+    {
+        "AreaName": "Greene",
+        "PercentChange": "-8.8"
+    },
+    {
+        "AreaName": "Guilford",
+        "PercentChange": "14.3"
+    },
+    {
+        "AreaName": "Halifax",
+        "PercentChange": "-14.7"
+    },
+    {
+        "AreaName": "Harnett",
+        "PercentChange": "13.5"
+    },
+    {
+        "AreaName": "Haywood",
+        "PercentChange": "6.7"
+    },
+    {
+        "AreaName": "Henderson",
+        "PercentChange": "4.8"
+    },
+    {
+        "AreaName": "Hertford",
+        "PercentChange": "-17.7"
+    },
+    {
+        "AreaName": "Hoke",
+        "PercentChange": "6.5"
+    },
+    {
+        "AreaName": "Hyde",
+        "PercentChange": "-37.2"
+    },
+    {
+        "AreaName": "Iredell",
+        "PercentChange": "13.4"
+    },
+    {
+        "AreaName": "Jackson",
+        "PercentChange": "1"
+    },
+    {
+        "AreaName": "Johnston",
+        "PercentChange": "31.7"
+    },
+    {
+        "AreaName": "Jones",
+        "PercentChange": "-21.7"
+    },
+    {
+        "AreaName": "Lee",
+        "PercentChange": "-5.9"
+    },
+    {
+        "AreaName": "Lenoir",
+        "PercentChange": "-8.1"
+    },
+    {
+        "AreaName": "Lincoln",
+        "PercentChange": "3.2"
+    },
+    {
+        "AreaName": "McDowell",
+        "PercentChange": "2.6"
+    },
+    {
+        "AreaName": "Macon",
+        "PercentChange": "-39.9"
+    },
+    {
+        "AreaName": "Madison",
+        "PercentChange": "-15.5"
+    },
+    {
+        "AreaName": "Martin",
+        "PercentChange": "-16.3"
+    },
+    {
+        "AreaName": "Mecklenburg",
+        "PercentChange": "16.8"
+    },
+    {
+        "AreaName": "Mitchell",
+        "PercentChange": "-5.7"
+    },
+    {
+        "AreaName": "Montgomery",
+        "PercentChange": "-19.1"
+    },
+    {
+        "AreaName": "Moore",
+        "PercentChange": "-10.1"
+    },
+    {
+        "AreaName": "Nash",
+        "PercentChange": "3.5"
+    },
+    {
+        "AreaName": "New Hanover",
+        "PercentChange": "-8.5"
+    },
+    {
+        "AreaName": "Northampton",
+        "PercentChange": "-24.9"
+    },
+    {
+        "AreaName": "Onslow",
+        "PercentChange": "1.4"
+    },
+    {
+        "AreaName": "Orange",
+        "PercentChange": "-1"
+    },
+    {
+        "AreaName": "Pamlico",
+        "PercentChange": "-21.5"
+    },
+    {
+        "AreaName": "Pasquotank",
+        "PercentChange": "-6.2"
+    },
+    {
+        "AreaName": "Pender",
+        "PercentChange": "-18.1"
+    },
+    {
+        "AreaName": "Perquimans",
+        "PercentChange": "-19.3"
+    },
+    {
+        "AreaName": "Person",
+        "PercentChange": "-6.8"
+    },
+    {
+        "AreaName": "Pitt",
+        "PercentChange": "5.3"
+    },
+    {
+        "AreaName": "Polk",
+        "PercentChange": "-24.4"
+    },
+    {
+        "AreaName": "Randolph",
+        "PercentChange": "7.7"
+    },
+    {
+        "AreaName": "Richmond",
+        "PercentChange": "-9.8"
+    },
+    {
+        "AreaName": "Robeson",
+        "PercentChange": "-18.9"
+    },
+    {
+        "AreaName": "Rockingham",
+        "PercentChange": "-5.2"
+    },
+    {
+        "AreaName": "Rowan",
+        "PercentChange": "2.6"
+    },
+    {
+        "AreaName": "Rutherford",
+        "PercentChange": "-14.2"
+    },
+    {
+        "AreaName": "Sampson",
+        "PercentChange": "-17.7"
+    },
+    {
+        "AreaName": "Scotland",
+        "PercentChange": "-5.7"
+    },
+    {
+        "AreaName": "Stanly",
+        "PercentChange": "6.2"
+    },
+    {
+        "AreaName": "Stokes",
+        "PercentChange": "-14.7"
+    },
+    {
+        "AreaName": "Surry",
+        "PercentChange": "-8.7"
+    },
+    {
+        "AreaName": "Swain",
+        "PercentChange": "36"
+    },
+    {
+        "AreaName": "Transylvania",
+        "PercentChange": "-19.7"
+    },
+    {
+        "AreaName": "Tyrrell",
+        "PercentChange": "-44.3"
+    },
+    {
+        "AreaName": "Union",
+        "PercentChange": "14.6"
+    },
+    {
+        "AreaName": "Vance",
+        "PercentChange": "-6.2"
+    },
+    {
+        "AreaName": "Wake",
+        "PercentChange": "11.9"
+    },
+    {
+        "AreaName": "Warren",
+        "PercentChange": "-17.1"
+    },
+    {
+        "AreaName": "Washington",
+        "PercentChange": "-18.5"
+    },
+    {
+        "AreaName": "Watauga",
+        "PercentChange": "145.7"
+    },
+    {
+        "AreaName": "Wayne",
+        "PercentChange": "-7.3"
+    },
+    {
+        "AreaName": "Wilkes",
+        "PercentChange": "-7.4"
+    },
+    {
+        "AreaName": "Wilson",
+        "PercentChange": "-5.1"
+    },
+    {
+        "AreaName": "Yadkin",
+        "PercentChange": "-8.1"
+    },
+    {
+        "AreaName": "Yancey",
+        "PercentChange": "-18.8"
+    }
+           
+] 
+}
+                   
+                       
+
